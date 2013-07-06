7a88dd71dea1ec54beb401c9193b8f95424a5de1
Leon Rosenberg <leon.rosenberg@anotheria.net>
2013-07-06 23:13:44 +0200

    checkstyle

--- java/org/moskito/control/core/Chart.java
+++ java/org/moskito/control/core/Chart.java
@@ -5,19 +5,33 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * TODO comment this class
+ * Represents a chart element of an application.
  *
  * @author lrosenberg
  * @since 18.06.13 23:01
  */
 public class Chart {
 
+	/**
+	 * Application this chart belongs to.
+	 */
 	private Application parent;
 
+	/**
+	 * Name of the chart.
+	 */
 	private String name;
 
+	/**
+	 * Chart lines. Chart line is a combination of component and accumulator.
+	 */
 	private List<ChartLine> lines = new LinkedList<ChartLine>();
 
+	/**
+	 * Creates a new chart.
+	 * @param aParent parent application.
+	 * @param aName name of the chart.
+	 */
 	public Chart(Application aParent, String aName){
 		name = aName;
 		parent = aParent;
@@ -43,6 +57,11 @@ public class Chart {
 		lines.add(new ChartLine(component, accumulator));
 	}
 
+	/**
+	 * Returns the list of accumulator names from given component required for this chart.
+	 * @param componentName name of the component.
+	 * @return list with accumulator names or empty list.
+	 */
 	public List<String> getNeededAccumulatorsForComponent(String componentName){
 		ArrayList<String> ret = new ArrayList<String>();
 		for (ChartLine line : lines){
