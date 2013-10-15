bf4e039982894dfe88d23e03d4782f2b803a6e3f
LotharII <lothariimail@gmail.com>
2013-10-15 18:06:24 +0300

    MSKCONTROL-46 Mail content changes.

--- java/org/moskito/control/mail/message/MailMessageBuilder.java
+++ java/org/moskito/control/mail/message/MailMessageBuilder.java
@@ -1,10 +1,9 @@
 package org.moskito.control.mail.message;
 
+import net.anotheria.util.NumberUtils;
 import org.moskito.control.core.StatusChangeEvent;
 import org.moskito.control.mail.MailServiceConfig;
 import org.moskito.control.mail.message.util.MessageCreationUtil;
-
-import java.util.Date;
 
 /**
  * Message builder for status changed mail.
@@ -14,17 +13,11 @@ import java.util.Date;
 public class MailMessageBuilder {
 
 	public static MailMessage buildStatusChangedMessage(StatusChangeEvent event, String recipient){
-		String content = "<b>"
-				+ new Date(event.getTimestamp())
-				+ "</b>: Application <b>"
-				+ event.getApplication()
-				+ "</b> component <b>"
-				+ event.getComponent()
-				+ "</b> has changed status from <b>"
-				+ event.getOldStatus()
-				+ "</b> to <b>"
-				+ event.getStatus()
-				+ "</b>";
+		String content = "Timestamp: <b>" + NumberUtils.makeISO8601TimestampString((event.getTimestamp())) + "</b><br/>"
+				+ "Application: <b>" + event.getApplication() + "</b><br/>"
+				+ "Component: <b>" + event.getComponent() + "</b><br/>"
+				+ "Old status: <b>" + event.getOldStatus() + "</b><br/>"
+				+ "New status: <b>" + event.getStatus() + "</b><br/>";
 		return  MessageCreationUtil.createHtmlMailMessage(recipient, content, MailServiceConfig.getInstance().getDefaultMessageSubject());
 	}
 }
