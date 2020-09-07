503928fa74ea6b4f426bf6c882d6db47ddd46cf0
ibohatyrchuk <bogiliv@gmail.com>
2020-09-07 23:24:45 +0300

    Plugability of history service. Improvements to InMemoryHistoryService.

--- core/src/main/java/org/moskito/control/core/history/service/InMemoryHistoryService.java
+++ core/src/main/java/org/moskito/control/core/history/service/InMemoryHistoryService.java
@@ -4,19 +4,26 @@ import org.moskito.control.config.MoskitoControlConfiguration;
 import org.moskito.control.core.history.StatusUpdateHistoryItem;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.Collectors;
 
 public class InMemoryHistoryService implements HistoryService {
-    private final List<StatusUpdateHistoryItem> items = new LinkedList<>();
+    private List<StatusUpdateHistoryItem> items = new CopyOnWriteArrayList<>();
 
     @Override
-    public synchronized void addItem(StatusUpdateHistoryItem historyItem) {
+    public void addItem(StatusUpdateHistoryItem historyItem) {
         items.add(historyItem);
-        while (items.size() > MoskitoControlConfiguration.getConfiguration().getHistoryItemsAmount()) {
-            items.remove(0);
+
+        int maxAllowedSize = (int) (MoskitoControlConfiguration.getConfiguration().getHistoryItemsAmount() * 1.10);
+        if (items.size() > maxAllowedSize) {
+            int fromIndex = Math.max(items.size() - 1 - MoskitoControlConfiguration.getConfiguration().getHistoryItemsAmount(), 0);
+            int toIndex = items.size();
+            StatusUpdateHistoryItem[] subList = Arrays.copyOfRange(items.toArray(new StatusUpdateHistoryItem[0]), fromIndex, toIndex);
+
+            this.items = new CopyOnWriteArrayList<>(subList);
         }
     }
 
