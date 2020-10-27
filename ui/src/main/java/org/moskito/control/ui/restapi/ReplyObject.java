aab1b46178afa7da837544ea3ea353a0d979bde6
Leon Rosenberg <leon.rosenberg@anotheria.net>
2020-10-27 13:59:33 +0100

    added structure for restapi v2 and added DebugResource

---
+++ ui/src/main/java/org/moskito/control/ui/restapi/ReplyObject.java
@@ -0,0 +1,126 @@
+package org.moskito.control.ui.restapi;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.HashMap;
+
+/**
+ * ReplyObject is a holder object for all rest api replies. It contains some status info and additional objects
+ * with requested information.
+ * This is basic reply object for version 2 of the api.
+ *
+ * @author lrosenberg
+ * @since 27.10.2020
+ */
+@XmlRootElement(name="reply")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ReplyObject {
+	/**
+	 * True if the call was successful.
+	 */
+	@XmlElement
+	private boolean success;
+	/**
+	 * Optional message in case call failed (exception message).
+	 */
+	@XmlElement(required = false,nillable = false)
+	private String message;
+
+	/**
+	 * Map with results object.
+	 */
+	@XmlElement
+	private HashMap<String, Object> results = new HashMap<>();
+
+	/**
+	 * Creates a new empty result object.
+	 */
+	public ReplyObject(){
+
+	}
+
+	/**
+	 * Creates a new result object with one result.
+	 * @param name name of the result bean.
+	 * @param result object for the first result bean.
+	 */
+	public ReplyObject(String name, Object result){
+		results.put(name, result);
+	}
+
+	/**
+	 * Adds
+	 * @param name
+	 * @param result
+	 */
+	public void addResult(String name, Object result){
+		results.put(name, result);
+	}
+
+	/**
+	 * Factory method that creates a new reply object for successful request.
+	 * @param name
+	 * @param result
+	 * @return
+	 */
+	public static ReplyObject success(String name, Object result){
+		ReplyObject ret = new ReplyObject(name, result);
+		ret.success = true;
+		return ret;
+	}
+
+	/**
+	 * Factory method that creates a new reply object for successful request.
+	 * @return
+	 */
+	public static ReplyObject success(){
+		ReplyObject ret = new ReplyObject();
+		ret.success = true;
+		return ret;
+	}
+
+	/**
+	 * Factory method that creates a new erroneous reply object.
+	 * @param message
+	 * @return
+	 */
+	public static ReplyObject error(String message){
+		ReplyObject ret = new ReplyObject();
+		ret.success = false;
+		ret.message = message;
+		return ret;
+	}
+
+	public static ReplyObject error(Throwable exc){
+		ReplyObject ret = new ReplyObject();
+		ret.success = false;
+		ret.message = exc.getClass().getSimpleName()+": "+exc.getMessage();
+		return ret;
+	}
+
+	@Override public String toString(){
+		StringBuilder ret = new StringBuilder("ReplyObject ");
+		ret.append("Success: ").append(success);
+		if (message!=null){
+			ret.append(", Message: ").append(message);
+		}
+		ret.append(", Results: ").append(results);
+		return ret.toString();
+	}
+
+	public boolean isSuccess(){
+		return success;
+	}
+
+	public String getMessage(){
+		return message;
+	}
+
+	public HashMap getResults(){
+		return results;
+	}
+
+
+}
---
+++ ui/src/main/java/org/moskito/control/ui/restapi/debug/DebugResource.java
@@ -0,0 +1,78 @@
+package org.moskito.control.ui.restapi.debug;
+
+import org.moskito.control.connectors.Connector;
+import org.moskito.control.connectors.ConnectorFactory;
+import org.moskito.control.connectors.response.ConnectorNowRunningResponse;
+import org.moskito.control.core.Component;
+import org.moskito.control.core.ComponentRepository;
+import org.moskito.control.ui.restapi.ReplyObject;
+import org.moskito.controlagent.data.nowrunning.EntryPoint;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+/**
+ * Used for providing additional debug information for development and testing purposes.
+ *
+ * @author lrosenberg
+ * @since 27.10.20 10:27
+ */
+@Path("debug")
+@Produces(MediaType.APPLICATION_JSON)
+public class DebugResource {
+	@GET
+	@Path("nowrunning")
+	public ReplyObject getNowRunning(){
+
+		ReplyObject ret = ReplyObject.success();
+
+		List<Component> componentList = ComponentRepository.getInstance().getComponents();
+		for (Component component : componentList){
+			Connector connector = ConnectorFactory.createConnector(component.getConfiguration().getConnectorType());
+			connector.configure(component.getConfiguration());
+			if (!connector.supportsNowRunning()){
+				continue;
+			}
+			ConnectorNowRunningResponse response = connector.getNowRunning();
+			List<EntryPoint> entryPoints = response.getEntryPoints();
+			if (entryPoints==null)
+				continue;
+			for (EntryPoint p : entryPoints){
+				ret.addResult(component.getName()+"."+p.getProducerId(), p.getCurrentMeasurements());
+			}
+
+		}
+
+		return ret;
+	}
+
+	@GET
+	@Path("nowrunningpast")
+	public ReplyObject getNowRunningPastMeasurements(){
+
+		ReplyObject ret = ReplyObject.success();
+
+		List<Component> componentList = ComponentRepository.getInstance().getComponents();
+		for (Component component : componentList){
+			Connector connector = ConnectorFactory.createConnector(component.getConfiguration().getConnectorType());
+			connector.configure(component.getConfiguration());
+			if (!connector.supportsNowRunning()){
+				continue;
+			}
+			ConnectorNowRunningResponse response = connector.getNowRunning();
+			List<EntryPoint> entryPoints = response.getEntryPoints();
+			if (entryPoints==null)
+				continue;
+			for (EntryPoint p : entryPoints){
+				ret.addResult(component.getName()+"."+p.getProducerId(), p.getPastMeasurements());
+			}
+
+		}
+
+		return ret;
+	}
+
+}
