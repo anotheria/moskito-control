c0cf460adea48dab614d25a534a130609043986c
Leon Rosenberg <leon.rosenberg@anotheria.net>
2020-11-23 11:24:08 +0100

    added builtin support for standard connectors, therefore removing them from configuration

--- connectors/src/main/java/org/moskito/control/connectors/ConnectorFactory.java
+++ connectors/src/main/java/org/moskito/control/connectors/ConnectorFactory.java
@@ -3,12 +3,15 @@ package org.moskito.control.connectors;
 import org.moskito.control.config.ConnectorConfig;
 import org.moskito.control.config.ConnectorType;
 import org.moskito.control.config.MoskitoControlConfiguration;
+import org.moskito.control.connectors.local.LocalMoSKitoConnector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Factory for creation of connector objects. This factory uses the ConnectorConfig entries from the configuration
  * to create Connector objects for different type of connectors. It reacts to configuration changes instantly.
+ * New in 3.x - the standard connector are now supported ootb and not needed to be configured. You can still override the
+ * standard connectors if you want to, but in 99% of the usages you won't need it.
  *
  * @author lrosenberg
  * @since 02.06.13 23:23
@@ -45,6 +48,25 @@ public final class ConnectorFactory {
 				}
 			}
 
+		}
+		return createBuiltInConnector(type);
+	}
+
+	private static Connector createBuiltInConnector(ConnectorType type){
+		switch (type){
+			case RMI:
+			case DISTRIBUTEME:
+				return new RMIConnector();
+			case HTTP:
+				return new HttpConnector();
+			case URL:
+				return new HttpURLConnector();
+			case MONGO:
+				return new MongoConnector();
+			case LOCALMOSKITO:
+				return new LocalMoSKitoConnector();
+			case JDBC:
+				return new JDBCConnector();
 		}
 		throw new IllegalArgumentException(type+" doesn't seem to be supported");
 	}
