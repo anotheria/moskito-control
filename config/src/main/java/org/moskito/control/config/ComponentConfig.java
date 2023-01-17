6f51ddb070df74a21d40ddf1fa41ba7b106e42f4
uniloftsky <uniloftsky@gmail.com>
2023-01-17 09:14:42 +0100

    URL Connector Extend - Extended ComponentConfig to accept method type, payload and content-type

--- config/src/main/java/org/moskito/control/config/ComponentConfig.java
+++ config/src/main/java/org/moskito/control/config/ComponentConfig.java
@@ -13,107 +13,158 @@ import org.configureme.annotations.ConfigureMe;
  */
 @ConfigureMe
 public class ComponentConfig {
-	private static final int COMPANY_NAME_LIMIT = 100;
+    private static final int COMPANY_NAME_LIMIT = 100;
 
-	/**
-	 * Name of the component.
-	 */
-	@Configure
-	@SerializedName("name")
-	private String name;
+    /**
+     * Name of the component.
+     */
+    @Configure
+    @SerializedName("name")
+    private String name;
 
-	/**
-	 * Category.
-	 */
-	@Configure
-	@SerializedName("category")
-	private String category;
+    /**
+     * Category.
+     */
+    @Configure
+    @SerializedName("category")
+    private String category;
 
-	/**
-	 * Type of the connector for this component.
-	 */
-	@Configure
-	@SerializedName("connectorType")
-	private ConnectorType connectorType;
+    /**
+     * Type of the connector for this component.
+     */
+    @Configure
+    @SerializedName("connectorType")
+    private ConnectorType connectorType;
 
-	/**
-	 * Connector specific location.
-	 */
-	@Configure
-	@SerializedName("location")
-	private String location;
+    /**
+     * Type of method if component is a URL-connector.
+     * If component is non URL-connector, field is ignored.
+     */
+    @Configure
+    @SerializedName("methodType")
+    private HttpMethodType methodType;
 
-	/**
-	 * Component tags.
-	 */
-	@Configure
-	@SerializedName("tags")
-	private String tags;
+    /**
+     * Payload. If a provided method type above accepts payload (POST, PUT etc.)
+     */
+    @Configure
+    @SerializedName("payload")
+    private String payload;
 
-	/**
-	 * Connector specific credentials.
-	 */
-	@Configure
-	private String credentials;
+    /**
+     * Content type. Field is required if payload was provided.
+     * Only text-friendly content type.
+     * Example: application/json, application/xml.
+     */
+    @Configure
+    @SerializedName("contentType")
+    private String contentType;
 
-	public String getName() {
-		return name;
-	}
+    /**
+     * Connector specific location.
+     */
+    @Configure
+    @SerializedName("location")
+    private String location;
 
-	public void setName(String name) {
+    /**
+     * Component tags.
+     */
+    @Configure
+    @SerializedName("tags")
+    private String tags;
+
+    /**
+     * Connector specific credentials.
+     */
+    @Configure
+    private String credentials;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         boolean isLengthLimitReached = !StringUtils.isEmpty(name) && name.length() > COMPANY_NAME_LIMIT;
         this.name = isLengthLimitReached ? name.substring(0, COMPANY_NAME_LIMIT) : name;
-	}
+    }
 
-	public String getCategory() {
-		return category;
-	}
+    public String getCategory() {
+        return category;
+    }
 
-	public void setCategory(String category) {
-		this.category = category;
-	}
+    public void setCategory(String category) {
+        this.category = category;
+    }
 
-	public ConnectorType getConnectorType() {
-		return connectorType;
-	}
+    public ConnectorType getConnectorType() {
+        return connectorType;
+    }
 
-	public void setConnectorType(ConnectorType connectorType) {
-		this.connectorType = connectorType;
-	}
+    public void setConnectorType(ConnectorType connectorType) {
+        this.connectorType = connectorType;
+    }
 
-	public String getLocation() {
-		return location;
-	}
+    public HttpMethodType getMethodType() {
+        return methodType;
+    }
 
-	public void setLocation(String location) {
-		this.location = location;
-	}
+    public void setMethodType(HttpMethodType methodType) {
+        this.methodType = methodType;
+    }
 
-	public String getCredentials() {
-		return credentials;
-	}
+    public String getPayload() {
+        return payload;
+    }
 
-	public void setCredentials(String credentials) {
-		this.credentials = credentials;
-	}
+    public void setPayload(String payload) {
+        this.payload = payload;
+    }
 
-	public String getTags() {
-		return tags;
-	}
+    public String getContentType() {
+        return contentType;
+    }
 
-	public void setTags(String tags) {
-		this.tags = tags;
-	}
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
 
-	@Override
-	public String toString() {
-		return "ComponentConfig{" +
-				"name='" + name + '\'' +
-				", category='" + category + '\'' +
-				", connectorType=" + connectorType +
-				", location='" + location + '\'' +
-				", tags='" + tags + '\'' +
-				", credentials='" + credentials + '\'' +
-				'}';
-	}
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getCredentials() {
+        return credentials;
+    }
+
+    public void setCredentials(String credentials) {
+        this.credentials = credentials;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    @Override
+    public String toString() {
+        return "ComponentConfig{" +
+                "name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                ", connectorType=" + connectorType +
+                ", methodType=" + methodType +
+                ", payload='" + payload + '\'' +
+                ", contentType='" + contentType + '\'' +
+                ", location='" + location + '\'' +
+                ", tags='" + tags + '\'' +
+                ", credentials='" + credentials + '\'' +
+                '}';
+    }
 }
