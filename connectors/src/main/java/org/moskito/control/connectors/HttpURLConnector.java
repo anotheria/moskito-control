d001c8e9f15d44305e5de1ff2d980d7a367c65e5
uniloftsky <uniloftsky@gmail.com>
2023-01-26 18:32:36 +0100

    URL Connector Extend - Added separated HttpURLConnector config

--- connectors/src/main/java/org/moskito/control/connectors/HttpURLConnector.java
+++ connectors/src/main/java/org/moskito/control/connectors/HttpURLConnector.java
@@ -1,5 +1,6 @@
 package org.moskito.control.connectors;
 
+import com.google.gson.annotations.SerializedName;
 import net.anotheria.moskito.core.accumulation.AccumulatedValue;
 import net.anotheria.moskito.core.accumulation.Accumulator;
 import net.anotheria.moskito.core.accumulation.AccumulatorRepository;
@@ -28,9 +29,13 @@ import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.entity.ContentType;
 import org.apache.http.message.BasicHeader;
+import org.configureme.ConfigurationManager;
+import org.configureme.annotations.Configure;
+import org.configureme.annotations.ConfigureMe;
 import org.moskito.control.config.ComponentConfig;
 import org.moskito.control.config.HeaderParameter;
 import org.moskito.control.config.HttpMethodType;
+import org.moskito.control.config.MoskitoControlConfiguration;
 import org.moskito.control.connectors.httputils.HttpHelper;
 import org.moskito.control.connectors.parsers.ParserHelper;
 import org.moskito.control.connectors.response.ConnectorAccumulatorResponse;
@@ -46,10 +51,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Generic Http URL connector.
@@ -101,7 +103,7 @@ public class HttpURLConnector extends AbstractConnector {
     private static Header gzipHeader = new BasicHeader(HttpHeaders.ACCEPT_ENCODING, "gzip");
 
     @Override
-    public void configure(ComponentConfig config){
+    public void configure(ComponentConfig config) {
         this.componentName = config.getName();
         this.location = config.getLocation();
         this.credentials = ParserHelper.getCredentials(config.getCredentials());
@@ -110,7 +112,7 @@ public class HttpURLConnector extends AbstractConnector {
         this.contentType = config.getContentType();
         HeaderParameter[] headers = config.getHeaders();
 
-        if(headers != null) {
+        if (headers != null) {
             this.headers = new Header[headers.length + 1];
             for (int i = 0; i < headers.length; i++) {
                 this.headers[i] = new BasicHeader(headers[i].getKey(), headers[i].getValue());
@@ -301,5 +303,126 @@ public class HttpURLConnector extends AbstractConnector {
     @Override
     public boolean supportsAccumulators() {
         return true;
+    }
+
+    @ConfigureMe(name = "http-url-connector")
+    public static class HttpURLConnectorConfig {
+
+        /**
+         * Type of method if component is a URL-connector.
+         * If component is non URL-connector, field is ignored.
+         */
+        @Configure
+        @SerializedName("methodType")
+        private HttpMethodType methodType;
+
+        /**
+         * Payload. If a provided method type above accepts payload (POST, PUT etc.)
+         */
+        @Configure
+        @SerializedName("payload")
+        private String payload;
+
+        /**
+         * Content type. Field is required if payload was provided.
+         * Only text-friendly content type.
+         * Example: application/json, application/xml.
+         */
+        @Configure
+        @SerializedName("contentType")
+        private String contentType;
+
+        /**
+         * Headers map. Used with HttpUrlConnector.
+         */
+        @Configure
+        @SerializedName("@headers")
+        private HeaderParameter[] headers;
+
+        public HttpMethodType getMethodType() {
+            return methodType;
+        }
+
+        public void setMethodType(HttpMethodType methodType) {
+            this.methodType = methodType;
+        }
+
+        public String getPayload() {
+            return payload;
+        }
+
+        public void setPayload(String payload) {
+            this.payload = payload;
+        }
+
+        public String getContentType() {
+            return contentType;
+        }
+
+        public void setContentType(String contentType) {
+            this.contentType = contentType;
+        }
+
+        public HeaderParameter[] getHeaders() {
+            return headers;
+        }
+
+        public void setHeaders(HeaderParameter[] headers) {
+            this.headers = headers;
+        }
+
+        /**
+         * Returns the active configuration instance. The configuration object will update itself if the config is changed on disk.
+         *
+         * @return configuration instance
+         */
+        public static final HttpURLConnectorConfig getConfiguration() {
+            return HttpURLConnectorConfig.HttpURLConnectorConfigHolder.instance;
+        }
+
+
+        /**
+         * Loads a new configuration object from disk. This method is for unit testing.
+         *
+         * @return configuration object
+         */
+        public static final HttpURLConnectorConfig loadConfiguration() {
+            HttpURLConnectorConfig config = new HttpURLConnectorConfig();
+            try {
+                ConfigurationManager.INSTANCE.configure(config);
+            } catch (IllegalArgumentException e) {
+                //ignored
+            }
+            return config;
+        }
+
+        /**
+         * Holder class for singleton instance.
+         */
+        private static class HttpURLConnectorConfigHolder {
+            /**
+             * Singleton instance of the MoskitoControlConfiguration object.
+             */
+            static final HttpURLConnectorConfig instance;
+
+            static {
+                instance = new HttpURLConnectorConfig();
+                try {
+                    ConfigurationManager.INSTANCE.configure(instance);
+                } catch (IllegalArgumentException e) {
+                    log.warn("can't find configuration - ensure you have moskitocontrol.json in the classpath");
+                }
+            }
+        }
+
+        @Override
+        public String toString() {
+            return "HttpURLConnectorConfig{" +
+                    "methodType=" + methodType +
+                    ", payload='" + payload + '\'' +
+                    ", contentType='" + contentType + '\'' +
+                    ", headers=" + Arrays.toString(headers) +
+                    '}';
+        }
     }
 }
