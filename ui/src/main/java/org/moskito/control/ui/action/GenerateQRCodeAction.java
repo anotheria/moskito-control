cf0a9106a5adf964aae1962707541256bf76a434
Leon Rosenberg <rosenberg.leon@gmail.com>
2025-05-14 17:07:35 +0200

    changed the way how the link is built for qr-code generation and also added some system.out

--- ui/src/main/java/org/moskito/control/ui/action/GenerateQRCodeAction.java
+++ ui/src/main/java/org/moskito/control/ui/action/GenerateQRCodeAction.java
@@ -40,7 +40,42 @@ public class GenerateQRCodeAction implements Action {
     }
 
     public static final String getQRCodeText(HttpServletRequest request){
-        return request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + "/api/v2";
+        String scheme = request.getHeader("X-Forwarded-Proto");
+        if (scheme == null) {
+            scheme = request.getScheme();
+        }
+        String host = request.getHeader("X-Forwarded-Host");
+        if (host == null) {
+            host = request.getServerName();
+        }else{
+            if (host.contains(":")) {
+                host = host.substring(0, host.indexOf(":")).trim();
+            }
+            if (host.contains(",")) {
+                host = host.substring(0, host.indexOf(",")).trim();
+            }
+        }
+
+        String port = request.getHeader("X-Forwarded-Port");
+        if (port == null) {
+            port = String.valueOf(request.getServerPort());
+
+        }
+
+        System.out.println("requestScheme:" +request.getScheme());
+        System.out.println("requestHost:" +request.getServerName());
+        System.out.println("requestPort:" +request.getServerPort());
+        System.out.println("X-Forwarded-Proto:" +request.getHeader("X-Forwarded-Proto"));
+        System.out.println("X-Forwarded-Host:" +request.getHeader("X-Forwarded-Host"));
+        System.out.println("X-Forwarded-Port:" +request.getHeader("X-Forwarded-Port"));
+
+        String portString = ":" + port;
+        if (port.equals("80") || port.equals("443")) {
+            portString = "";
+        }
+
+        String text = scheme + "://" + host + portString + "/api/v2";
+        return text;
     }
 
     private static byte[] generateQRCodeImage(String text) throws Exception {
